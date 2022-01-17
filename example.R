cbe2df334601ca6237d476c3f94b7b83001cfbc5
frankhan12345 <39461916+frankhan12345@users.noreply.github.com>
2022-01-17 15:16:37 +0800

    Fix bug

--- example.R
+++ example.R
@@ -2,19 +2,22 @@ library(rgl)
 library(ggplot2)
 
 # Define the target PDF (2-dimensional Rastrigin function within range [-2,2]*[-2,2])
-pdf = function(x) {
+pdf <- function(x) {
   (20 + x[1]^2 + x[2]^2 - 10*(cos(2*pi*x[1]) + cos(2*pi*x[2])))/362.6667
 }
 
 # View the shape of the target PDF
-curv_mesh = expand.grid(x=seq(-2,2,length.out=1e2),y=seq(-2,2,length.out=1e2),z=NA)
+curv_mesh <- expand.grid(x=seq(-2, 2, length.out=1e2), y=seq(-2, 2, length.out=1e2), z=NA)
 for (i in 1:nrow(curv_mesh)) {
-  curv_mesh$z[i] = pdf(c(curv_mesh$x[i],curv_mesh$y[i]))
+  curv_mesh$z[i] <- pdf(c(curv_mesh$x[i], curv_mesh$y[i]))
 }
 plot3d(x=curv_mesh$x,y=curv_mesh$y,z=curv_mesh$z)
 
 # Sample
-s = mhsample(c(0,0), 1e4, pdf=pdf, proppdf=function(x,y) 1/16, proprnd=function(x) c(runif(1,-2,2),runif(1,-2,2)))
+s <- mhsample(c(0.5, 0.5), 1e5,
+              pdf=pdf,
+              proprnd=function(x) c(runif(1, -2, 2), runif(1, -2, 2)),
+              symmetric=TRUE)
 
 # View the sampling result
 ggplot() +
--- mhsample.R
+++ mhsample.R
@@ -8,56 +8,58 @@
 #   symmetric: a logical value indicating whether the proposal distribution is symmetric. The default value is FALSE. If it is TRUE (i.e., proppdf is symmetric), proppdf is optional.
 #   burnin: burnin number. The default value is 0.
 #   thin: thin number. The default value is 1.
-#   show: a logical value indicating whether to show the real-time progress
+#   show: a logical value indicating whether to show the progress bar. The progress bar will greatly increase the running time of this function, which can be alleviated by using a large "showinterval" argument.
+#   showinterval: an integer indicating the sample interval to update the progress bar.
 # Output:
-#   smpl: generated samples
-mhsample = function(start, nsample, ...) {
+#   smpl: generated samples.
+mhsample <- function(start, nsample, ...) {
   # Parse input
-  varargin = list(...)
-  pdf = varargin$pdf
-  proppdf = varargin$proppdf
-  proprnd = varargin$proprnd
-  sym = if (is.null(varargin$symmetric)) FALSE else varargin$symmetric
-  burnin = if (is.null(varargin$burnin)) 0 else varargin$burnin
-  thin = if (is.null(varargin$thin)) 1 else varargin$thin
-  show = if (is.null(varargin$show)) FALSE else varargin$show
-  
-  nDim = length(start)
+  varargin <- list(...)
+  pdf <- varargin$pdf
+  proppdf <- varargin$proppdf
+  proprnd <- varargin$proprnd
+  sym <- if (is.null(varargin$symmetric)) FALSE else varargin$symmetric
+  burnin <- if (is.null(varargin$burnin)) 0 else varargin$burnin
+  thin <- if (is.null(varargin$thin)) 1 else varargin$thin
+  show <- if (is.null(varargin$show)) FALSE else varargin$show
+  showinterval <- if (is.null(varargin$showinterval)) 1 else varargin$showinterval
+
+  if (!is.array(start)) {
+    start <- array(start, c(1,length(start)))
+  }
+  nDim <- ncol(start)
   
   # Perform Metropolis-Hasting algorithm
-  smpl = array(0, dim = c(burnin + nsample * thin, nDim))
-  x0 = start
-  i = 1
+  smpl <- array(0, dim = c(nsample, nDim))
+  x0 <- start
+  accept <- 0
+  U <- log(runif(nsample * thin + burnin))
   if (show){
     library(tcltk)
-    pb = tkProgressBar(max = nrow(smpl))
+    pb <- tkProgressBar(title = sprintf('%d / %d', 0, nsample * thin + burnin),
+                        label = sprintf('Sampling: %d / %d', 0, nsample * thin + burnin),
+                        max = nsample * thin + burnin, initial = 0)
   }
-  while (i <= nrow(smpl)) {
-    y = proprnd(x0)
+  for (i in (1 - burnin):(nsample * thin)) {
+    y <- array(proprnd(x0), c(1,nDim))
     if (sym) {
-      rho = pdf(y) / pdf(x0)
+      rho <- log(pdf(y)) - log(pdf(x0))
     } else {
-      rho = pdf(y) / pdf(x0) * proppdf(x0,y) / proppdf(y,x0)
+      rho <- log(pdf(y)) - log(pdf(x0)) + log(proppdf(x0, y)) - log(proppdf(y, x0))
     }
-    acc = min(1,rho)
-    u = runif(1)
-    if (u<acc) {
-      smpl[i,] = y
-      x0 = y
-      if (show) {
-        if (!environment(pb[["getVal"]])[[".killed"]])
-          setTkProgressBar(pb, value = i, title = sprintf('%d / %d', i, nrow(smpl)), label = sprintf('Sampling: %d / %d', i, nrow(smpl)))
-      }
-      i = i + 1
+    Ui <- U[i + burnin]
+    acc <- Ui <= min(0, rho)
+    x0[acc,] <- y[acc,]
+    accept <- accept + acc
+
+    if (i>0 && i%%thin==0) { # burnin and thin
+      smpl[i/thin,] <- x0
+    }
+    if (show && (i+burnin)%%showinterval==0) {
+      if (!environment(pb[["getVal"]])[[".killed"]])
+        setTkProgressBar(pb, value = i + burnin, title = sprintf('%d / %d', i + burnin, nsample * thin + burnin), label = sprintf('Sampling: %d / %d', i + burnin, nsample * thin + burnin))
     }
   }
-  
-  # Burnin
-  smpl = smpl[(1+burnin):nrow(smpl),]
-  smpl = array(smpl, dim = c(NROW(smpl), NCOL(smpl)))
-  
-  # Thin
-  smpl = smpl[seq(thin,nsample*thin,thin),]
   
   # Clean and Return
   if (show) {
